750ec6bb28ea1a6fbde2cbe2f2ef95cff51ed4ed
aslih <aslih@DESKTOP-MSVDGHF>
2021-05-02 15:39:16 +0300

    02.05.2021 || 15:40

--- oopIntro/src/oopIntro/main.java
+++ oopIntro/src/oopIntro/main.java
@@ -6,7 +6,6 @@ public class main {
 		
 		Product product1 = new Product(1,"Lenova V14", 15000, "16 GB Ram",10); 
         
-		//github denemesi
 		Product product2 = new Product(); 
 	    product2.setId(2);
 		product2.setName("Lenova V15");
